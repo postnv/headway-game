9f50f1c2843277c34573b5a34e8ac1e12c117f6e
Yan <yan.postnov.main@gmail.com>
2024-07-29 18:34:00 +0300

    chore: prepare json-server to deploy

--- src/page-content/game/api/loadQuestions.ts
+++ src/page-content/game/api/loadQuestions.ts
@@ -1,6 +1,8 @@
+import { APP_API_URL } from "@/src/shared/config";
+
 export default async function loadQuestions() {
   try {
-    const response = await fetch("http://localhost:4000/questions");
+    const response = await fetch(`${APP_API_URL}/questions`);
     const questions = await response.json();
 
     return questions;
--- src/page-content/game/game.tsx
+++ src/page-content/game/game.tsx
@@ -1,7 +1,7 @@
-import { loadQuestions } from "@/src/shared/api";
 import { GameProvider } from "@/src/entities/game";
 import { GameProgress } from "@/src/widgets/game-progress";
 import { Game } from "@/src/widgets/game";
+import { loadQuestions } from "./api";
 
 import styles from "./styles.module.scss";
 
